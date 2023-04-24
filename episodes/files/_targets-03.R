7ebe2f7c5e575e7b1cf181ef3302b268b1e99368
carpenter <team@carpentries.org>
2023-04-24 09:13:15 +0900

    Use external files for targets plans

---
+++ episodes/files/_targets-03.R
@@ -0,0 +1,23 @@
+library(targets)
+# This is an example _targets.R file. Every
+# {targets} pipeline needs one.
+# Use tar_script() to create _targets.R and tar_edit()
+# to open it again for editing.
+# Then, run tar_make() to run the pipeline
+# and tar_read(summary) to view the results.
+
+# Define custom functions and other global objects.
+# This is where you write source(\"R/functions.R\")
+# if you keep your functions in external scripts.
+summ <- function(dataset) {
+  colSums(dataset)
+}
+
+# Set target-specific options such as packages:
+# tar_option_set(packages = "utils") # nolint
+
+# End this file with a list of target objects.
+list(
+  tar_target(my_data, data.frame(x = sample.int(100), y = sample.int(100))),
+  tar_target(my_summary, summ(my_data)) # Call your custom functions as needed.
+)
